185a8da39bc60e37665875396d03a22a6441d7fa
yaniky <295958179@qq.com>
2019-09-17 10:38:24 +0800

    version(0.0.3): 修改构建后图片和字体资源存放目录

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -66,7 +66,13 @@ module.exports = {
                     {
                         loader: "url-loader",
                         options: {
-                            limit: 10000
+                            limit: 10000,
+                            fallback: {
+                                loader:'file-loader',
+                                options: {
+                                    name: 'assets/images/[name].[ext]'
+                                }
+                            }
                         }
                     }
                 ]
@@ -84,7 +90,13 @@ module.exports = {
                     {
                         loader: "url-loader",
                         options: {
-                            limit: 100000
+                            limit: 100000,
+                            fallback: {
+                                loader:'file-loader',
+                                options: {
+                                    name: 'assets/font/[name].[ext]'
+                                }
+                            }
                         }
                     }
                 ]
